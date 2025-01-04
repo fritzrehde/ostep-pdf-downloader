f69e570c2fc899057c5ecae8d08274409423ee56
fritzrehde <fritzrehde.work@gmail.com>
2025-01-05 00:30:02 +1100

    progress

--- ostep_pdf_scraper/__init__.py
+++ ostep_pdf_scraper/__init__.py
@@ -34,17 +34,14 @@ class Book:
 
 
 def parse_chapter(
-    chapter_pdf_path: str,
-) -> Tuple[Chapter, int, Optional[Tuple[str, int]]]:
+    chapter_pdf_path: str, starting_page_num: int
+) -> Tuple[Chapter, int]:
     """
-    Return the parsed chapter, the number of pages in this chapter and, if this
-    chapter starts a new part, the title and start page of the part.
+    Return the parsed chapter and the number of pages in this chapter.
     """
     doc = pymupdf.open(chapter_pdf_path)
     page_count = len(doc)
 
-    new_part_title = None
-    new_part_page_num = None
     chapter_idx = None
     chapter_title = None
     chapter_page_num = None
@@ -61,20 +58,19 @@ def parse_chapter(
                     font_size: float = span["size"]
                     text: str = span["text"].strip()
 
-                    if font_size >= 10:
-                        print(font_size, text)
+                    # if font_size >= 10:
+                    #     print(font_size, text)
 
                     # We identify headings by their (large) font sizes.
-                    if 17 < font_size < 20:
-                        new_part_title = text
-                        new_part_page_num = page_num
                     if 20 < font_size and text.isdigit():
                         chapter_idx = int(text)
-                    if 12 < font_size < 15:
+                    elif 12 < font_size < 15:
                         chapter_title = text
-                        chapter_page_num = page_num
+                        chapter_page_num = starting_page_num + page_num
                     elif 10 < font_size < 12:
-                        subchapter = SubChapter(text, page_num)
+                        subchapter = SubChapter(
+                            text, starting_page_num + page_num
+                        )
                         subchapters.append(subchapter)
 
     assert chapter_title is not None and chapter_page_num is not None
@@ -83,49 +79,48 @@ def parse_chapter(
         chapter_title = f"{chapter_idx} {chapter_title}"
 
     chapter = Chapter(chapter_title, chapter_page_num, subchapters)
-    maybe_new_part = (
-        (new_part_title, new_part_page_num)
-        if new_part_title is not None and new_part_page_num is not None
-        else None
-    )
 
-    return chapter, page_count, maybe_new_part
+    return chapter, page_count
 
 
-def parse_chapters(chapter_pdf_paths: List[str]) -> List[Chapter]:
-    page_num = 0
-
-    chapters_iter = iter(map(parse_chapter, chapter_pdf_paths))
-
-    parts: List[Part] = []
-
-    def parse_part():
-        pass
-
-    while next(chapters_iter, None) is not None:
-        curr_part_title = None
-        curr_part_page_num = None
-
-        chapters_in_part: List[Chapter] = []
-
-        # TODO: adjust the page numbers
-
-        for chapter, page_count, maybe_new_part in chapters_iter:
-            match maybe_new_part:
-                case (new_part_title, new_part_page_num):
-                    pass
-                case None:
-                    pass
-
-            page_num += page_count
+def scrape_parts_chapter_files() -> List[Tuple[str, List[str]]]:
+    # TODO: scrapce https://pages.cs.wisc.edu/~remzi/OSTEP/ and create a temp dir with files and return (kinda) map from part title to the chapter pdf file paths
 
     return []
 
 
+def parse_book() -> Book:
+    page_num = 0
+
+    parts: List[Part] = []
+    # TODO: init tempfile here
+    for part_title, chapter_pdf_paths in scrape_parts_chapter_files():
+        # Assume the part starts at the first page of its first chapter.
+        part_page_num = page_num
+
+        chapters: List[Chapter] = []
+        for path in chapter_pdf_paths:
+            chapter, page_count = parse_chapter(path, page_num)
+            chapters.append(chapter)
+            page_num += page_count
+
+        part = Part(part_title, part_page_num, chapters)
+        parts.append(part)
+
+    title = ""
+    author = ""
+    description = ""
+    book = Book(title, author, description, parts)
+
+    return book
+
+
 def main():
     # print(parse_chapter("/home/fritz/Downloads/cpu-intro.pdf", 0))
-    print(parse_chapter("/home/fritz/Downloads/dialogue-virtualization.pdf", 0))
+    # print(parse_chapter("/home/fritz/Downloads/dialogue-virtualization.pdf", 0))
     # print(parse_chapter("/home/fritz/Downloads/toc.pdf", 0))
+
+    book = parse_book()
 
 
 if __name__ == "__main__":
